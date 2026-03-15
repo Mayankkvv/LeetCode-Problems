48249d05a211717a5db15bad3f2ae0ded24e69a5
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-03-15 12:08:08 +0530

    Time: 17 ms (48.05%), Space: 47.3 MB (20.7%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,50 @@
+class Solution {
+private:
+    vector<int> findnse(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> nse(n);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+            nse[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return nse;
+    }
+
+    vector<int> findpsee(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> psee(n);
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+            psee[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return psee;
+    }
+
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> nse = findnse(arr);
+        vector<int> psee = findpsee(arr);
+
+        long long total = 0;
+        int mod = 1e9 + 7;
+
+        for (int i = 0; i < n; i++) {
+            long long left = i - psee[i];
+            long long right = nse[i] - i;
+            total = (total + (left * right % mod) * arr[i] % mod) % mod;
+        }
+
+        return (int)total;
+    }
+};
