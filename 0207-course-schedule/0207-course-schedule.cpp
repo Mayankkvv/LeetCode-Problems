fcb3bfacf5940bdc94fa701db0e09d5b94ee1761
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-05-16 22:07:54 +0530

    Time: 6 ms (50.4%), Space: 19.2 MB (66.23%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+
+    bool canFinish(int numCourses,
+                   vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> adj(numCourses);
+
+        // Build graph
+        for (int i = 0; i < prerequisites.size(); i++) {
+
+            adj[prerequisites[i][1]]
+                .push_back(prerequisites[i][0]);
+        }
+
+        vector<int> indegree(numCourses, 0);
+
+        // Calculate indegree
+        for (int i = 0; i < numCourses; i++) {
+
+            for (auto it : adj[i]) {
+
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+
+        // Push indegree 0 nodes
+        for (int i = 0; i < numCourses; i++) {
+
+            if (indegree[i] == 0) {
+
+                q.push(i);
+            }
+        }
+
+        vector<int> topo;
+
+        // Kahn's Algorithm
+        while (!q.empty()) {
+
+            int node = q.front();
+
+            q.pop();
+
+            topo.push_back(node);
+
+            for (auto it : adj[node]) {
+
+                indegree[it]--;
+
+                if (indegree[it] == 0) {
+
+                    q.push(it);
+                }
+            }
+        }
+
+        return topo.size() == numCourses;
+    }
+};
