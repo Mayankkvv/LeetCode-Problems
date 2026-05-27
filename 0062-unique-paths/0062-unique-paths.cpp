b82de20b701f1b21b50fb3849440cbb6056ca5df
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-05-27 21:46:12 +0530

    Time: 0 ms (100%), Space: 9.4 MB (50.84%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    int f(int m, int n, int i, int j, vector<vector<int>>& dp){
+        if (i >= m || j >= n){
+            return 0;
+        }
+        if((i == m -1) && (j == n -1)){
+            return 1;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int right = f(m, n, i , j+ 1, dp);
+        int left = f(m, n, i+ 1, j, dp);
+        return dp[i][j] = left + right;
+    }
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        return f(m,n,0,0,dp);
+    }
+};
