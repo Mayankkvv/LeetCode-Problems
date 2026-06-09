c856e3cc13c6b93643644568c4810a548b4810ed
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-06-09 16:56:43 +0530

    Time: 11 ms (26.12%), Space: 24.8 MB (23.82%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,23 +1,36 @@
 class Solution {
 private:
-    int f(int ind, int buy, vector<int>& prices, int n, vector<vector<int>>& dp){
-        if(ind == n) return 0;
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-        int a = 0, b= 0;
-        if(buy){
-            a = max((-prices[ind] + f(ind +1 , 0, prices, n, dp)), 
+    int f(int ind, int buy, vector<int>& prices, int n,
+          vector<vector<int>>& dp) {
+        if (ind == n)
+            return 0;
+        if (dp[ind][buy] != -1)
+            return dp[ind][buy];
+        int a = 0, b = 0;
+        if (buy) {
+            a = max((-prices[ind] + f(ind + 1, 0, prices, n, dp)),
                     (0 + f(ind + 1, 1, prices, n, dp)));
-        }
-        else{
-            b = max((prices[ind] + f(ind + 1, 1, prices, n, dp)) ,
+        } else {
+            b = max((prices[ind] + f(ind + 1, 1, prices, n, dp)),
                     (0 + f(ind + 1, 0, prices, n, dp)));
         }
         return dp[ind][buy] = max(a, b);
     }
+
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return f(0, 1, prices, n, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        dp[n][0] = 0, dp[n][1] = 0;
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy < 2; buy++) {
+                if (buy) {
+                    dp[ind][buy] = max((-prices[ind] + dp[ind + 1][0]), (0 + dp[ind + 1][1]));
+                } else {
+                    dp[ind][buy] = max((prices[ind] + dp[ind + 1][1]), (0 + dp[ind + 1][0]));
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
