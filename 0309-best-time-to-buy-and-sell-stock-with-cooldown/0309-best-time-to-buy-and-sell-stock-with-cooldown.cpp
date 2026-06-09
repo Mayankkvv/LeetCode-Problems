8f334571762cf91fa0d2815d7b83669f2c6d3955
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-06-09 21:51:50 +0530

    Time: 0 ms (100%), Space: 16.1 MB (59.71%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& Arr) {
+        int n = Arr.size();
+        // Create a 2D DP array with dimensions (n+2) x 2, initialized to 0
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+
+        // Loop through the stock prices from the end to the beginning
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit;
+
+                if (buy == 0) { // We can buy the stock
+                    profit =
+                        max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
+                }
+
+                if (buy == 1) { // We can sell the stock
+                    profit = max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 2][0]);
+                }
+
+                // Store the computed profit in the DP array
+                dp[ind][buy] = profit;
+            }
+        }
+
+        return dp[0][0];
+    }
+};
