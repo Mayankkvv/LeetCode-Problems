01de3c75d9a863b95ea8cb74103592d9e18384fa
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-06-25 23:09:28 +0530

    Time: 0 ms (100%), Space: 34.4 MB (50.89%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -1,52 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
-    
-    TreeNode* helper(TreeNode* root){
-        
-        // no left child
-        if(root->left == nullptr){
-            return root->right;
+private:
+    TreeNode* findLast(TreeNode* root){
+        if(root -> right == NULL){
+            return root;
         }
-
-        // no right child
-        if(root->right == nullptr){
-            return root->left;
-        }
-
-        // both children exist
-        TreeNode* rightChild = root->right;
-        TreeNode* lastRight = root->left;
-
-        while(lastRight->right){
-            lastRight = lastRight->right;
-        }
-
-        lastRight->right = rightChild;
-
-        return root->left;
+        return findLast(root -> right);
     }
-
-    TreeNode* deleteNode(TreeNode* root, int key) {
-
-        if(root == nullptr) return nullptr;
-        if(root->val == key){
-            return helper(root);
+    TreeNode* f(TreeNode* root){
+        if(root -> left == NULL){
+            return root -> right;
         }
+        else if(root -> right == NULL){
+            return root -> left;
+        }
+        TreeNode* rightChild = root -> right;
+        TreeNode* lastRight = findLast(root -> left);
+        lastRight -> right = rightChild;
+        return root -> left;
+    }
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
         TreeNode* cur = root;
+        if(root == NULL) return NULL;
+        if(root -> val == key) return f(root);
         while(cur){
-            if(key < cur->val){
-                if(cur->left && cur->left->val == key){
-                    cur->left = helper(cur->left);
+            if(cur -> val > key){
+                if(cur -> left != NULL && cur -> left -> val == key){
+                    cur -> left = f(cur -> left);
                     break;
+                }else{
+                    cur = cur -> left;
                 }
-                cur = cur->left;
             }
             else{
-                if(cur->right && cur->right->val == key){
-                    cur->right = helper(cur->right);
+                if(cur -> right != NULL && cur -> right -> val == key){
+                    cur -> right = f(cur -> right);
                     break;
+                }else{
+                    cur = cur -> right;
                 }
-                cur = cur->right;
             }
         }
         return root;
