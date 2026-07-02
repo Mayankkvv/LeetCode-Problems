4b9f8f44084e823a7ca68adf76e49854968d27f4
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-07-02 10:03:13 +0530

    Time: 5 ms (31.59%), Space: 18.4 MB (64.23%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    //T.C : O(N), N =. total nodes in all lists
+    //S,C : O(N)
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+        auto lambda = [](ListNode* node1, ListNode* node2) {
+            return node1->val > node2->val;
+        };
+
+        priority_queue<ListNode*, vector<ListNode*>, decltype(lambda)> pq(lambda); 
+
+        for(ListNode* node : lists) {
+            if(node != NULL) {
+                pq.push(node);
+            }
+        }
+
+        if(pq.empty()) {
+            return NULL;
+        }
+
+        ListNode* head = pq.top(); //head of final merged list
+        pq.pop();
+
+        if(head->next != NULL) {
+            pq.push(head->next);
+        }
+
+        ListNode* tail = head;
+
+        while(!pq.empty()) {
+            ListNode* curr = pq.top();
+            pq.pop();
+
+            tail->next = curr;
+            tail = tail->next;
+
+            if(curr->next != NULL)
+                pq.push(curr->next);
+        }
+
+        return head;
+
+    }
+};
