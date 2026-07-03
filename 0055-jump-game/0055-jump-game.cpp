ba1dd061bf72d1d58c8ca0ea53ec7c7447f3271e
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-07-03 16:46:22 +0530

    Time: 0 ms (100%), Space: 52.4 MB (26.73%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,37 +1,11 @@
 class Solution {
-private:
-    bool f(int start, vector<int>& nums, int end, vector<int>& dp) {
-
-        // Reached or crossed end
-        if (start >= end)
-            return true;
-
-        // Memoization
-        if (dp[start] != -1)
-            return dp[start];
-
-        int jumps = nums[start];
-
-        // Try all jumps
-        for (int i = 1; i <= jumps; i++) {
-
-            if (start + i < nums.size()) {
-
-                if (f(start + i, nums, end, dp))
-                    return dp[start] = true;
-            }
-        }
-
-        return dp[start] = false;
-    }
-
 public:
     bool canJump(vector<int>& nums) {
-
-        int n = nums.size();
-
-        vector<int> dp(n, -1);
-
-        return f(0, nums, n - 1, dp);
-    }
+        int maxIndex = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(i > maxIndex) return false;
+            maxIndex = max(maxIndex, i+nums[i]);
+        }
+        return true;
+    }   //TC -> O(N)   SC -> O(1)
 };
