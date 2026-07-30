98b820f08ef2bf5397873e52ab1ea12d240d80d2
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-07-30 22:59:02 +0530

    Time: 19 ms (52.9%), Space: 26.2 MB (44.18%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,64 +1,33 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-
+        queue<pair<int, pair<int,int>>> q;
         int n = grid.size();
         int m = grid[0].size();
-
-        // blocked start or end
-        if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1) {
-
-            return -1;
-        }
-
-        // single cell
-        if (n == 1 && m == 1) {
-
-            return 1;
-        }
-
-        vector<vector<int>> dist(n, vector<int>(m, 1e9));
-
-        queue<pair<int, pair<int, int>>> q;
-
-        q.push({1, {0, 0}});
-
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
+        if (n == 1) return 1;
+        vector<vector<int>> dist(n, vector<int>(m,1e9));
         dist[0][0] = 1;
-
-        int dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
-        int dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
-
-        while (!q.empty()) {
-
+        q.push({1,{0,0}});
+        int drow[8] = {-1,-1,0,1,1,1,0,-1};
+        int dcol[8] = {0,1,1,1,0,-1,-1,-1};
+        while(!q.empty()){
             auto it = q.front();
-
             q.pop();
-
+            int row = it.second.first;
+            int col = it.second.second;
             int dis = it.first;
+            for(int i =0; i < 8; i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
 
-            int r = it.second.first;
-            int c = it.second.second;
-
-            for (int i = 0; i < 8; i++) {
-
-                int nrow = r + dx[i];
-                int ncol = c + dy[i];
-
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                    grid[nrow][ncol] == 0 && dis + 1 < dist[nrow][ncol]) {
-
-                    if (nrow == n - 1 && ncol == m - 1) {
-
-                        return dis + 1;
-                    }
-
-                    dist[nrow][ncol] = dis + 1;
-
-                    q.push({dis + 1, {nrow, ncol}});
+                if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m && grid[nrow][ncol] == 0 && dis + 1 < dist[nrow][ncol]){
+                    dist[nrow][ncol] = 1 + dis;
+                    if(nrow == n-1 && ncol == m-1) return dis + 1;
+                    q.push({1+dis, {nrow, ncol}});
                 }
             }
         }
-
         return -1;
     }
 };
