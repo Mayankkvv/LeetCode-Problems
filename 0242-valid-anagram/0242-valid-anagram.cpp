705692a2340679fafcf88896d18eb8a7a1d1586c
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-08-09 00:51:39 +0530

    Time: 0 ms (100%), Space: 9.6 MB (92.96%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        int arr1[26] = {0};
+        int arr2[26] = {0};
+        for(int i = 0; i < t.size(); i++){
+            arr1[t[i] - 'a']++;
+            arr2[s[i] - 'a']++;
+        }
+        for(int i = 0; i < 26; i++){
+            if(arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
+};
