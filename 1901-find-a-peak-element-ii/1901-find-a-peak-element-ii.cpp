ec5aab0be544e9b7c37a8a3d46d272bf5215244a
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-08-22 10:50:10 +0530

    Time: 0 ms (100%), Space: 50.2 MB (73.4%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,11 +1,10 @@
 class Solution {
 private:
-    int f(vector<vector<int>>& mat, int n, int m , int col){
-        int maxVal = -1;
-        int index = -1;
+    int FindMaxInCol(vector<vector<int>>& mat, int col, int n){
+        int maxi = INT_MIN, index = -1;
         for(int i = 0; i < n; i++){
-            if(mat[i][col] > maxVal){
-                maxVal = mat[i][col];
+            if(maxi < mat[i][col]){
+                maxi = mat[i][col];
                 index = i;
             }
         }
@@ -15,20 +14,19 @@ public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        int low = 0, high = m -1;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            int maxRowIndex = f(mat, n, m , mid);
-            int left = mid - 1>=0 ? mat[maxRowIndex][mid-1]:-1;
-            int right = mid + 1 <m ? mat[maxRowIndex][mid+1]:-1;
-            if(mat[maxRowIndex][mid] > left && mat[maxRowIndex][mid] > right){
-                return {maxRowIndex, mid};
+        int start = 0, end = m - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            int MaxRow = FindMaxInCol(mat, mid, n);
+            int left = mid - 1 >= 0 ? mat[MaxRow][mid-1] : -1;
+            int right = mid + 1 < m ? mat[MaxRow][mid+1] : -1;
+            if(mat[MaxRow][mid] > left && mat[MaxRow][mid] > right){
+                return {MaxRow, mid};
             }
-            else if(mat[maxRowIndex][mid] < left){
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
+            else if(mat[MaxRow][mid] < left){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
             }
         }
         return {-1,-1};
