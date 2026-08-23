04ff070086be679f9fb69db1831b8c3d76c7b1d8
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-08-23 23:17:17 +0530

    Time: 0 ms (100%), Space: 12.8 MB (63.03%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> next;
-        stack<int> s;
-
-        for (int i = nums2.size() - 1; i >= 0; --i) {
-            while (!s.empty() && s.top() <= nums2[i]) {
-                s.pop();
+        int n = nums2.size();
+        vector<int> nge(n, 0);
+        stack<int> st;
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums2[i]){
+                st.pop();
             }
-            next[nums2[i]] = s.empty() ? -1 : s.top();
-            s.push(nums2[i]);
+            if(st.empty()) nge[i] = -1;
+            else nge[i] = st.top();
+            st.push(nums2[i]);
         }
-
-        vector<int> res;
-        for (int num : nums1) {
-            res.push_back(next[num]);
+        unordered_map<int, int> mp;
+        for(int i = 0; i < n; i++) {
+            mp[nums2[i]] = nge[i];
         }
-        return res;
+        vector<int> ans;
+        for(int i = 0; i < nums1.size(); i++){
+            ans.push_back(mp[nums1[i]]);
+        }
+        return ans;
     }
 };
