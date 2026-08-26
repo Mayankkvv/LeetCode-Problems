4ccba188d57470d666d158398a704fca30145bae
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-08-26 12:40:07 +0530

    Time: 61 ms (81%), Space: 193.6 MB (35.38%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,50 +1,42 @@
 class Solution {
 private:
-    vector<int> findnse(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> nse(n);
+    vector<int> findNse(vector<int>& arr){
         stack<int> st;
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
+        vector<int> nse(arr.size(), 0);
+        for(int i = arr.size() - 1; i>= 0; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                 st.pop();   
             }
-            nse[i] = st.empty() ? n : st.top();
+            if(st.empty()) nse[i] = arr.size();
+            else nse[i] = st.top();
             st.push(i);
         }
         return nse;
     }
-
-    vector<int> findpsee(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> psee(n);
+    vector<int> findPsee(vector<int>& arr){
+        vector<int> psee(arr.size(), 0);
         stack<int> st;
-
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && arr[st.top()] > arr[i]) {
+        for(int i = 0;i<arr.size(); i++){
+            while(!st.empty() && arr[st.top()] > arr[i]){
                 st.pop();
             }
-            psee[i] = st.empty() ? -1 : st.top();
+            if(st.empty()) psee[i] = -1;
+            else psee[i] = st.top();
             st.push(i);
         }
         return psee;
     }
-
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> nse = findnse(arr);
-        vector<int> psee = findpsee(arr);
-
         long long total = 0;
-        int mod = 1e9 + 7;
-
-        for (int i = 0; i < n; i++) {
+        long long mod = (int)(1e9 + 7);
+        vector<int> nse = findNse(arr);
+        vector<int> psee = findPsee(arr);
+        for(int i = 0; i < arr.size(); i++){
             long long left = i - psee[i];
             long long right = nse[i] - i;
-            total = (total + (left * right % mod) * arr[i] % mod) % mod;
+            total = (total + (left * right * arr[i])%mod)%mod;
         }
-
-        return (int)total;
+        return total;
     }
 };
