142eac2decfb970390a518384810adebe5caa5b7
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-08-28 11:47:42 +0530

    Time: 0 ms (100%), Space: 11.3 MB (75.03%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,16 +1,11 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     string removeKdigits(string nums, int k) {
-        
         stack <char> st;
         for(int i=0; i < nums.size(); i++) {
             char digit = nums[i];
             while(!st.empty() && k > 0
                   && st.top() > digit) {
-
                 st.pop(); 
                 k--;
             }
@@ -38,4 +33,3 @@ public:
         return res;
     }
 };
-
