221d4495ea198610ad415d483d54a780d69e63ca
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-09-02 10:39:53 +0530

    Time: 2 ms (36.14%), Space: 10.5 MB (71.58%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,21 +1,34 @@
 class Solution {
 private:
-    void f(int ind, vector<vector<int>>& ans, vector<int> nums){
-        if(ind == nums.size()){
-            ans.push_back(nums);
-            return ;
+    void f(int ind, vector<vector<int>>& ans, vector<int>& ds,
+           vector<int>& vis, vector<int>& nums, int n) {
+        // If permutation is complete
+        if(ds.size() == n) {
+            ans.push_back(ds);
+            return;
         }
-        for(int i = ind; i < nums.size(); i++){
-            swap(nums[ind], nums[i]);
-            f(ind+1, ans,nums);
-            swap(nums[i], nums[ind]);
+        // Try every element
+        for(int i = 0; i < n; i++) {
+            // If already used, skip it
+            if(vis[i] == 1)
+                continue;
+            // Choose
+            vis[i] = 1;
+            ds.push_back(nums[i]);
+            // Recursion
+            f(i + 1, ans, ds, vis, nums, n);
+            // Backtrack
+            ds.pop_back();
+            vis[i] = 0;
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> ans;
-        //vector<int> ds;
-        f(0,ans,nums);
+        vector<int> ds;
+        vector<int> vis(n, 0);
+        f(0, ans, ds, vis, nums, n);
         return ans;
     }
 };
