9334ad2671e8863f83f0a1d5feef7877fda706fe
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-09-15 11:49:09 +0530

    Time: 2 ms (21.01%), Space: 9.4 MB (30.4%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -10,7 +10,16 @@ private:
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(0, 0, m, n, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        dp[m-1][n-1] = 1;
+        for(int i = m - 1; i >= 0 ; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i == m - 1 && j == n - 1) continue;
+                int down = dp[i][j+1];
+                int right = dp[i+1][j];
+                dp[i][j] = down + right;
+            }
+        }
+        return dp[0][0];
     }
 };
