682edbd952e1e78dbf6a2b1a1cfce8b0076c0abe
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-09-15 18:07:43 +0530

    Time: 3 ms (13.29%), Space: 13.2 MB (58.32%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,31 +1,27 @@
 class Solution {
+private:
+    int f(int i,int j, int rows,vector<vector<int>>& triangle, vector<vector<int>>& dp){
+        if(i == rows) return triangle[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        int a = triangle[i][j] + f(i + 1, j, rows, triangle, dp);
+        int b = triangle[i][j] + f(i+1, j+ 1, rows, triangle, dp);
+        return dp[i][j] = min(a,b);
+    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-
-        int n = triangle.size();
-
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-
-        // Base case
-        for (int j = 0; j < n; j++) {
-            dp[n - 1][j] = triangle[n - 1][j];
+        int rows = triangle.size();
+        vector<vector<int>> dp(rows, vector<int>(rows,0));
+        for(int i = 0; i < rows ; i++){
+            dp[rows-1][i] = triangle[rows-1][i];
         }
-
-        // Bottom-up
-        for (int i = n - 2; i >= 0; i--) {
-
-            for (int j = 0; j <= i; j++) {
-
-                int down =
-                    triangle[i][j] + dp[i + 1][j];
-
-                int diagonal =
-                    triangle[i][j] + dp[i + 1][j + 1];
-
-                dp[i][j] = min(down, diagonal);
+        for(int i = rows - 2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                int a = triangle[i][j] + dp[i+1][j];
+                int b = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(a,b);
             }
         }
-
         return dp[0][0];
+        //return f(0,0,rows - 1,triangle, dp);
     }
 };
