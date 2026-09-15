135a83f55d75f7de4200f0cc5b07d4e960fae3f9
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-09-15 17:09:16 +0530

    Time: 2 ms (35.09%), Space: 16.2 MB (61.56%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,29 +1,34 @@
 class Solution {
-private:
-    int sum(int i, int j, vector<vector<int>>& grid, int n, int m,
-            vector<vector<int>>& dp) {
-
-        // Out of bounds
-        if (i >= n || j >= m)
-            return INT_MAX;
-
-        // Destination
-        if (i == n - 1 && j == m - 1)
-            return grid[i][j];
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int right = sum(i, j + 1, grid, n, m,dp);
-        int down = sum(i + 1, j, grid, n, m,dp);
-        return dp[i][j] = grid[i][j] + min(right, down);
-    }
-
 public:
     int minPathSum(vector<vector<int>>& grid) {
 
         int n = grid.size();
         int m = grid[0].size();
+
         vector<vector<int>> dp(n, vector<int>(m, -1));
-        return sum(0, 0, grid, n, m, dp);
+        dp[n - 1][m - 1] = grid[n - 1][m - 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+
+                if (i == n - 1 && j == m - 1) {
+                    dp[i][j] = grid[i][j];
+                } else {
+
+                    int right = INT_MAX;
+                    int down = INT_MAX;
+
+                    if (j + 1 < m)
+                        right = grid[i][j] + dp[i][j + 1];
+
+                    if (i + 1 < n)
+                        down = grid[i][j] + dp[i + 1][j];
+
+                    dp[i][j] = min(right, down);
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
