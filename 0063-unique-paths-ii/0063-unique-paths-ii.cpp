c742d8935255ca4ebb90eed23c32c0d65e96b2d1
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-09-15 12:32:52 +0530

    Time: 3 ms (9.54%), Space: 12 MB (30.88%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 private:
-    int f(int i, int j, vector<vector<int>>& obstacleGrid, int n, int m,
-          vector<vector<int>>& dp){
-        if(i >= n || j >= m) return 0;
-        if(obstacleGrid[i][j] == 1) return 0;
-        if(i == n -1 && j == m -1) return 1;
+    int f(int i, int j, int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp){
+        if(i >= row || j >= col || grid[i][j] == 1) return 0;
+        if(i == row - 1 && j == col - 1) return 1;
         if(dp[i][j] != -1) return dp[i][j];
-        int right = f(i, j+1, obstacleGrid, n , m,dp);
-        int down = f(i+1, j, obstacleGrid, n, m,dp);
+        int down = f(i+1, j, row, col, grid, dp);
+        int right = f(i, j+1, row, col, grid, dp);
         return dp[i][j] = down + right;
     }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m,-1));
-        return f(0,0,obstacleGrid, n, m,dp);
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(0,0,m,n, obstacleGrid, dp);
     }
 };
