bdd7e24fc2dce5c62b64b1959a6bb98f1489b06f
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-09-24 09:49:09 +0530

    Time: 172 ms (23.33%), Space: 111.3 MB (19.23%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,44 +1,26 @@
 class Solution {
 private:
-    bool f(int i, int n, vector<int>& nums, int target, vector<vector<int>>& dp) {
-
-        // Subset found
-        if (target == 0)
-            return true;
-
-        // Reached end
-        if (i == n)
-            return false;
-
+    bool f(int i, int target, vector<int>& nums, int n, vector<vector<int>>& dp){
+        if(target == 0) return true;
+        if(i >= n) return false;
+        if(i == n-1) return (target == nums[n-1]);
         if(dp[i][target] != -1) return dp[i][target];
-
-        bool pick = false;
-
-        if (nums[i] <= target) {
-
-            pick = f(i + 1, n, nums, target - nums[i], dp);
+        bool take = false;
+        if(nums[i] <= target){
+            take = f(i+1, target-nums[i], nums, n, dp);
         }
-
-        bool notpick = f(i + 1, n, nums, target, dp);
-
-        return dp[i][target] = pick || notpick;
+        bool nottake = f(i+1, target, nums, n, dp);
+        return dp[i][target] = take||nottake;
     }
-
 public:
     bool canPartition(vector<int>& nums) {
-
         int n = nums.size();
-        int totsum = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            totsum += nums[i];
+        int sum =0;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
         }
-        vector<vector<int>> dp(n, vector<int>(totsum + 1, -1));
-
-        // Odd sum cannot be partitioned
-        if (totsum % 2 == 1)
-            return false;
-
-        return f(0, n, nums, totsum / 2,dp);
+        if(sum % 2 == 1) return false;
+        vector<vector<int>> dp(n+1, vector<int>(sum/2 + 1, -1));
+        return f(0, sum/2, nums,n,dp);
     }
 };
